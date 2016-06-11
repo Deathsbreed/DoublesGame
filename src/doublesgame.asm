03beb2f1a8ac71f667da159f338f6163d590a1c9
Nicolás A. Ortega <nicolas.ortega.froysa@gmail.com>
2016-06-11 06:42:18 +0200

    Added game description.

--- src/doublesgame.asm
+++ src/doublesgame.asm
@@ -1,3 +1,16 @@
+##
+# DoublesGame
+# Copyright (c) 2016 Nicolás A. Ortega
+# License: GNU GPLv3
+#
+# Description:
+#   DoublesGame is a small game where you are given 2^X where X is a number
+# that continuously increases (starting from 1). You then have to enter the
+# correct answer to the equation in order to continue. There are three
+# other responses aside from giving a number answer to the equation, these
+# are a `help' command, which displays the two other commands, the `answer'
+# command which will allow you to skip a double giving you the correct
+# answer, and the `exit' command which will close the game.
 .section .data
     welcome:
         .ascii "Welcome to the doubles game.\n"
